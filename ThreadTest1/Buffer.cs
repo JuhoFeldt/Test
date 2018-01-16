64cd63e1b1206ad5cfc75620713ab43b60717174
Feldt <Juho.Feldt@seamk.fi>
2018-01-16 11:25:21 +0200

    eka versio

---
+++ ThreadTest1/Buffer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadTest1
+{
+    class Buffer
+    {
+
+        private Queue<int> queue = new Queue<int>();
+
+        private static object objToLock = new object();
+
+        public void Put(int data)
+        {
+            Monitor.Enter(objToLock);
+
+            queue.Enqueue(data);
+            Monitor.Pulse(objToLock);
+            Monitor.Exit(objToLock);
+        }
+
+        public int Get()
+        {
+            Monitor.Enter(objToLock);
+
+            Console.WriteLine("Queue count: "+queue.Count);
+            while(queue.Count==0)
+            {
+                Monitor.Wait(objToLock);
+            }
+            int data = queue.Dequeue();
+
+            Monitor.Pulse(objToLock);
+            Monitor.Exit(objToLock);
+
+            return data;
+        }
+
+    }
+}
---
+++ ThreadTest1/Worker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadTest1
+{
+    /*
+    class Measurements
+    {
+        public double TimeStamp { get; set; }
+        public double MeasA { get; set; }
+        public double MeasB { get; set; }
+    }*/
+
+    class Worker
+    {
+        private volatile bool shouldStop = false;
+        private Buffer buffer = new Buffer();
+
+        // thread method
+        public void Producer() // producer will update measurements
+        {
+            int counter = 0;
+            Random gen = new Random();
+            while (!shouldStop)
+            {
+                buffer.Put(counter);
+                counter++;
+                if(counter%2==0)
+                    Thread.Sleep(2);
+                else
+                Thread.Sleep(gen.Next() % 2000);
+            }
+            Console.WriteLine("worker thread A: terminating gracefully");
+        }
+
+        // thread method
+        public void Consumer() // consumer will write measurements to the console
+        {
+            Random gen = new Random();
+            Thread.Sleep(150);
+            while (!shouldStop)
+            {
+                int data = buffer.Get();
+                Console.WriteLine(data);
+                Thread.Sleep(gen.Next() % 1000);
+            }
+            Console.WriteLine("worker thread B: terminating gracefully");
+        }
+
+        public void RequestStop()
+        {
+            shouldStop = true;
+        }
+    }
+}
